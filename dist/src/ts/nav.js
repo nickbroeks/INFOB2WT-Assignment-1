5dd2ac5fe2ddf419db958373cf4b7a42a9825c48
Nick Broeks <nick.broeks@live.nl>
2024-02-27 01:05:58 +0100

    Changed index

--- dist/src/ts/nav.js
+++ dist/src/ts/nav.js
@@ -1,16 +1,16 @@
 "use strict";
 // When the user scrolls the page, execute myFunction
 window.onscroll = function () {
-    const header = document.getElementById('nav-bar');
+    const header = document.getElementById('js-nav-bar');
     if (header) {
         const sticky = header.offsetTop;
         if (window.scrollY > sticky) {
-            header.classList.remove('nav-transparent');
-            header.classList.add('nav-solid');
+            header.classList.remove('js-nav-bar--transparent');
+            header.classList.add('js-nav-bar--solid');
         }
         else {
-            header.classList.add('nav-transparent');
-            header.classList.remove('nav-solid');
+            header.classList.add('js-nav-bar--transparent');
+            header.classList.remove('js-nav-bar--solid');
         }
     }
 };
