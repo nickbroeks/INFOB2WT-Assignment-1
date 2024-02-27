aaa8d6b6de2049ba1a82e2d8567004f0f3b55371
Jack <jackchen8888@gmail.com>
2024-02-27 21:17:24 +0100

    undo false commit

--- dist/src/ts/nav.js
+++ dist/src/ts/nav.js
@@ -1,16 +1,16 @@
 "use strict";
 // When the user scrolls the page, execute myFunction
 window.onscroll = function () {
-    const header = document.getElementById('js-nav-bar');
+    const header = document.getElementById('nav-bar');
     if (header) {
         const sticky = header.offsetTop;
         if (window.scrollY > sticky) {
-            header.classList.remove('js-nav-bar--transparent');
-            header.classList.add('js-nav-bar--solid');
+            header.classList.remove('nav-transparent');
+            header.classList.add('nav-solid');
         }
         else {
-            header.classList.add('js-nav-bar--transparent');
-            header.classList.remove('js-nav-bar--solid');
+            header.classList.add('nav-transparent');
+            header.classList.remove('nav-solid');
         }
     }
 };
