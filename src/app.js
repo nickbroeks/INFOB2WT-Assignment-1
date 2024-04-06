dd081427ca278b0b02cc8ae1afc69a87d2c0ad8e
Nick Broeks <nick.broeks@live.nl>
2024-04-06 17:25:18 +0200

    redirect index.html

--- src/app.js
+++ src/app.js
@@ -16,6 +16,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/index.html', (req, res) => {
+    res.redirect('/');
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
